0119cc12d72338a6288ee30318ca0dbfe9e764f6
perevernihata <ivan.perevernyhata@gmail.com>
2013-11-15 01:13:23 -0800

    - put in order dll references

---
+++ Charting/OxyPlotCharting/OxyPlotAdapter.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Linq;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.WindowsForms;
+using LineSeries = OxyPlot.Series.LineSeries;
+using LinearAxis = OxyPlot.Axes.LinearAxis;
+
+namespace FreeChartTools.Charting.OxyPlotCharting
+{
+    public class OxyPlotAdapter : BaseChartAdapter
+    {
+        public OxyPlotAdapter(ChartParameters parameters)
+            : base(parameters)
+        {
+
+        }
+
+        protected override Image DoCreateChartImage()
+        {
+            var plotModel = new PlotModel("");
+            var ls = new LineSeries("")
+                {
+                    Points = Parameters.SeriaData.Select(t => new DataPoint(t.Key, t.Value) as IDataPoint).ToList()
+                };
+            plotModel.Series.Add(ls);
+            plotModel.Axes.Add(new LinearAxis(AxisPosition.Left, -4, 4));
+            plotModel.Axes.Add(new LinearAxis(AxisPosition.Bottom));
+            var bitmap = new Bitmap(Parameters.ChartWidth, Parameters.ChartHeight);
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                var graphicsRenderContext1 = new GraphicsRenderContext();
+                graphicsRenderContext1.RendersToScreen = false;
+                var graphicsRenderContext2 = graphicsRenderContext1;
+                graphicsRenderContext2.SetGraphicsTarget(graphics);
+                plotModel.Update();
+                plotModel.Render(graphicsRenderContext2, Parameters.ChartWidth, Parameters.ChartHeight);
+                return bitmap;
+            }
+        }
+    }
+}
---
+++ Charting/OxyPlotCharting/OxyPlotChartFactory.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FreeChartTools.Charting.OxyPlotCharting
+{
+    public class OxyPlotChartFactory: BaseChartFactory
+    {
+        public override IChartAdapter DoGenerateChart(ChartParameters parameters)
+        {
+            return new OxyPlotAdapter(parameters);
+        }
+
+        public override string ChartTypeName
+        {
+            get { return "Oxy Plot"; }
+        }
+    }
+}
