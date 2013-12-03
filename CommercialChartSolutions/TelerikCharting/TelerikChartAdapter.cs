edeb89f8b242be681eb2c1705b70fbed23a007f8
Ivan <ivan.perevernyhata@gmail.com>
2013-12-03 13:52:42 -0800

    - implemented Telerik provider

---
+++ CommercialChartSolutions/TelerikCharting/TelerikChartAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using ChartingCore;
+using Telerik.Charting;
+using Telerik.Web.UI;
+
+namespace TelerikCharting
+{
+    public class TelerikChartAdapter: BaseChartAdapter
+    {
+        public TelerikChartAdapter(ChartParameters parameters) : base(parameters)
+        {
+        }
+
+        protected override Image DoCreateChartImage()
+        {
+            var radChart = new RadChart();
+            radChart.ChartTitle.TextBlock.Text = "Test Telerik Chart";
+
+            var tmpSeries = new ChartSeries() { Type = ChartSeriesType.Line };
+            foreach (var point in Parameters.SeriaData)
+                tmpSeries.Items.Add(new ChartSeriesItem(point.Key, point.Value));
+            radChart.AddChartSeries(tmpSeries);
+            var r = new Random(radChart.GetHashCode());
+            return radChart.GetBitmap();
+        }
+    }
+}
---
+++ CommercialChartSolutions/TelerikCharting/TelerikChartFactory.cs
@@ -0,0 +1,17 @@
+﻿using ChartingCore;
+
+namespace TelerikCharting
+{
+    public class TelerikChartFactory: BaseChartFactory
+    {
+        public override IChartAdapter DoGenerateChart(ChartParameters parameters)
+        {
+            return new TelerikChartAdapter(parameters);
+        }
+
+        public override string ChartTypeName
+        {
+            get { return "Telerik charts"; }
+        }
+    }
+}
