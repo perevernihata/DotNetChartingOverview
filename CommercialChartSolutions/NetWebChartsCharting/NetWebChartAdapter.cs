dc1874503d0f2e92e1a240943fb254c4cc59d163
Ivan <ivan.perevernyhata@gmail.com>
2013-12-04 04:07:21 -0800

    - implemented ChartDirector provider

---
+++ CommercialChartSolutions/NetWebChartsCharting/NetWebChartAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using ChartDirector;
+using ChartingCore;
+
+namespace NetWebChartsCharting
+{
+    /// <summary>
+    /// You can download trial NetWebChart binaries from here: http://www.advsofteng.com/download.html
+    /// </summary>
+    public class NetWebChartAdapter: BaseChartAdapter
+    {
+        public NetWebChartAdapter(ChartParameters parameters) : base(parameters)
+        {
+        }
+
+        protected override Image DoCreateChartImage()
+        {
+            var chart = new XYChart(Parameters.ChartWidth, Parameters.ChartHeight);
+            chart.setPlotArea(30, 20, (int)Math.Round(Parameters.ChartWidth - (Parameters.ChartWidth * 0.2)), (int)Math.Round(Parameters.ChartHeight - (Parameters.ChartHeight * 0.2)));
+            chart.addLineLayer(Parameters.SeriaData.Select(p => (double)p.Key).ToArray());
+            chart.xAxis().setLabelStep(3);
+            return chart.makeImage();
+        }
+    }
+}
---
+++ CommercialChartSolutions/NetWebChartsCharting/NetWebChartFactory.cs
@@ -0,0 +1,20 @@
+﻿using ChartingCore;
+
+namespace NetWebChartsCharting
+{
+    /// <summary>
+    /// You can download trial NetWebChart binaries from here: http://www.advsofteng.com/download.html
+    /// </summary>
+    public class NetWebChartFactory : BaseChartFactory
+    {
+        public override IChartAdapter DoGenerateChart(ChartParameters parameters)
+        {
+            return new NetWebChartAdapter(parameters);
+        }
+
+        public override string ChartTypeName
+        {
+            get { return "ChartDirector"; }
+        }
+    }
+}
