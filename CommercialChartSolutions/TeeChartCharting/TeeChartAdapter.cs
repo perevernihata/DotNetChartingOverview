ff391030da45bbe7bf36c0fc25c511450f939226
Ivan <ivan.perevernyhata@gmail.com>
2013-12-04 03:30:58 -0800

    - implemented TeeChart provider

---
+++ CommercialChartSolutions/TeeChartCharting/TeeChartAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using ChartingCore;
+using Steema.TeeChart;
+using Steema.TeeChart.Styles;
+
+namespace TeeChartCharting
+{
+    public class TeeChartAdapter: BaseChartAdapter
+    {
+        public TeeChartAdapter(ChartParameters parameters) : base(parameters)
+        {
+        }
+
+        protected override Image DoCreateChartImage()
+        {
+            var chart = new Chart();
+            var seria = new Line(chart);
+            foreach (var point in Parameters.SeriaData)
+                seria.Add(point.Key, point.Value);
+            chart.Series.Add(seria);
+            return chart.Bitmap(Parameters.ChartWidth, Parameters.ChartHeight);
+        }
+    }
+}
---
+++ CommercialChartSolutions/TeeChartCharting/TeeChartFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ChartingCore;
+
+namespace TeeChartCharting
+{
+    public class TeeChartFactory: BaseChartFactory
+    {
+        public override IChartAdapter DoGenerateChart(ChartParameters parameters)
+        {
+            return new TeeChartAdapter(parameters);
+        }
+
+        public override string ChartTypeName
+        {
+            get { return "Tee charts"; }
+        }
+    }
+}
