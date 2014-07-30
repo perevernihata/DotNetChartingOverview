3e64be5414d74f96dc485ba624a1d7c3c9a51074
Ivan <ivan.perevernyhata@gmail.com>
2014-07-30 16:09:36 +0300

    - introduced dotnetCharting solution

---
+++ CommercialChartSolutions/dotnetCharting/dotnetChartAdapter.cs
@@ -0,0 +1,40 @@
+﻿using ChartingCore;
+using dotnetCHARTING;
+using System.Drawing;
+
+namespace dotnetChartingLibrary
+{
+    /// <summary>
+    /// Free for non-commercial use
+    /// </summary>
+    public class DotnetChartAdapter : BaseChartAdapter
+    {
+        public DotnetChartAdapter(ChartParameters parameters)
+            : base(parameters)
+        {
+        }
+
+        protected override Image DoCreateChartImage()
+        {
+            var chart = new Chart();
+            chart.Type = ChartType.Combo;
+            chart.Width = Parameters.ChartWidth;
+            chart.Height = Parameters.ChartHeight;
+            chart.Series.Data = Parameters.SeriaData;
+            var seriesCollection = new SeriesCollection();
+            var s = new Series("", SeriesType.Line);
+            foreach (var pair in Parameters.SeriaData)
+            {
+                s.Elements.Add(new Element()
+                {
+                    YValue = pair.Key,
+                    XValue = pair.Value
+                });
+            }
+            seriesCollection.Add(s);
+            chart.SeriesCollection.Add(seriesCollection);
+
+            return chart.GetChartBitmap();
+        }
+    }
+}
---
+++ CommercialChartSolutions/dotnetCharting/dotnetChartFactory.cs
@@ -0,0 +1,31 @@
+﻿using ChartingCore;
+
+namespace dotnetChartingLibrary
+{
+    /// <summary>
+    /// Commercial
+    /// </summary>
+    public class DotnetChartFactory : BaseChartFactory
+    {
+        public override IChartAdapter DoGenerateChart(ChartParameters parameters)
+        {
+            return new DotnetChartAdapter(parameters);
+        }
+
+        public override string ChartTypeName
+        {
+            get { return "dotnetCharting"; }
+        }
+
+        public override string DownloadLink
+        {
+            get { return "http://www.dotnetcharting.com/download.aspx"; }
+        }
+
+        public override SolutionType SolutionType
+        {
+            get { return SolutionType.Commercial; }
+        }
+
+    }
+}
