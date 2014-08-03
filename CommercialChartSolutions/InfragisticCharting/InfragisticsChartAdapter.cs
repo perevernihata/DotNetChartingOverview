1d1c36c2900854f48b94c68ba08954e1eda1d09e
Ivan <ivan.perevernyhata@gmail.com>
2014-08-03 12:51:44 +0300

    - introduced Infragistics charts

---
+++ CommercialChartSolutions/InfragisticCharting/InfragisticsChartAdapter.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using ChartingCore;
+using Infragistics.UltraChart.Resources.Appearance;
+using Infragistics.UltraChart.Shared.Styles;
+using Infragistics.WebUI.UltraWebChart;
+using System.IO;
+
+namespace InfragisticCharting
+{
+    /// <summary>
+    /// Commercial
+    /// http://www.infragistics.com/products/aspnet
+    /// </summary>
+    public class InfragisticsChartAdapter : BaseChartAdapter
+    {
+        public InfragisticsChartAdapter(ChartParameters parameters)
+            : base(parameters)
+        {
+        }
+
+        protected override Image DoCreateChartImage()
+        {
+            var chart = new UltraChart {ChartType = ChartType.LineChart};
+            var sc1 = new NumericSeries();
+            foreach (var valuePair in Parameters.SeriaData)
+            {
+                sc1.Points.Add(new NumericDataPoint(valuePair.Key, valuePair.Value.ToString(), false));
+            }
+            chart.Series.Add(sc1);
+            chart.Height = Parameters.ChartHeight;
+            chart.Width = Parameters.ChartWidth;
+            using (var stream = new MemoryStream())
+            {
+                chart.SaveTo(stream, ImageFormat.Png);
+                return new Bitmap(stream);
+            }
+        }
+    }
+}
---
+++ CommercialChartSolutions/InfragisticCharting/InfragisticsFactory.cs
@@ -0,0 +1,31 @@
+﻿using ChartingCore;
+
+namespace InfragisticCharting
+{
+    /// <summary>
+    /// Commercial
+    /// http://www.infragistics.com/products/aspnet
+    /// </summary>
+    public class InfragisticsFactory : BaseChartFactory
+    {
+        public override IChartAdapter DoGenerateChart(ChartParameters parameters)
+        {
+            return new InfragisticsChartAdapter(parameters);
+        }
+
+        public override string ChartTypeName
+        {
+            get { return "Infragistics chart"; }
+        }
+
+        public override string DownloadLink
+        {
+            get { return "http://www.infragistics.com/products/aspnet"; }
+        }
+
+        public override SolutionType SolutionType
+        {
+            get { return SolutionType.Commercial; }
+        }
+    }
+}
